61918f11bfc37b1af5ba2c0a68318febe4e645af
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-04-07 10:10:18 -0300

    Aula 9 finalizada. Expressões. Substituir corpo do if por ternário e armazenar numa constante.

---
+++ 09-expressoes/script.js
@@ -0,0 +1,28 @@
+const grupoA = 100
+const grupoB = 300
+
+const numeros = [2, 3, 4, 5, 6]
+
+
+
+
+// Expressão
+const vencedor = grupoA > grupoB ? 'Grupo A Ganhou' : 'Grupo B Ganhou'
+
+// Expressão
+const grupoAvenceu = grupoA > 50 && 'Grupo A Vencedor'
+
+// Expressão
+const areaQuadrado = l => l * l
+
+// Expressão
+const total = numeros.filter(numero => numero > 4)
+
+
+
+
+
+console.log(vencedor)
+console.log(areaQuadrado(4))
+console.log(total)
+console.log(grupoAvenceu)
