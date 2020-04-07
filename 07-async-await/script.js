922651ce09deee14b37d8b089f5fc364f4be2d8c
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-04-06 22:07:13 -0300

    Aula 7 finalizada. Usando async e await no lugar de .then()

---
+++ 07-async-await/script.js
@@ -0,0 +1,11 @@
+async function fetchProdutos(url) {
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    return jsonBody
+}
+
+const requisicao = fetchProdutos('https://ranekapi.origamid.dev/wp-json/api/produto')
+
+requisicao.then(response => {
+    console.log(response)
+})
